00753490564cc37ff40658ec0d235193a2ab93d3
shyam <mvmshyam@gmail.com>
2021-08-19 19:30:14 +0530

    bfs implementation

--- BST.js
+++ BST.js
@@ -57,8 +57,82 @@ class BST {
       return currentNode;
     }
   }
+  BFS() {
+    let visited = [];
+    let queue = [];
+    if (!this.root) {
+      return visited;
+    }
+    queue.push(this.root);
+    while (queue.length) {
+      let currentNode = queue.shift();
+      visited.push(currentNode.value);
+      if (currentNode.left) {
+        queue.push(currentNode.left);
+      }
+      if (currentNode.right) {
+        queue.push(currentNode.right);
+      }
+    }
+    return visited;
+  }
+
+  DFSPreOrder() {
+    let visited = [];
+    let currentNode = this.root;
+    if (!currentNode) {
+      return visited;
+    }
+
+    function traverse(node) {
+      visited.push(node.value);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    }
+    traverse(currentNode);
+    return visited;
+  }
+
+  DFSPostOrder() {
+    let visited = [];
+    let currentNode = this.root;
+    if (!currentNode) {
+      return visited;
+    }
+
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      visited.push(node.value);
+    }
+    traverse(currentNode);
+    return visited;
+  }
+
+  DFSInOrder() {
+    let visited = [];
+    let currentNode = this.root;
+    if (!currentNode) {
+      return visited;
+    }
+
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      visited.push(node.value);
+      if (node.right) traverse(node.right);
+    }
+    traverse(currentNode);
+    return visited;
+  }
 }
 
 const tree = new BST();
-console.log(tree.insert(10).insert(9).insert(15).insert(3));
-console.log(tree.find(10));
+tree.insert(10).insert(6).insert(15).insert(3).insert(8).insert(20);
+// console.log(tree.find(10));
+console.log(tree.BFS());
+console.log(tree.DFSPreOrder());
+console.log(tree.DFSPostOrder());
+console.log(tree.DFSInOrder());
+
+// BFS -- use it for left skew or right skew tree (space complefixty will be 1 in this case);
+//BFS/DFS  time complexity same in both cases
